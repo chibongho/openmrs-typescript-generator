ed444296cdd4d60b4e88323a5f028e49b1f64867
Chi Bong Ho <cbho@pih.org>
2024-09-09 07:38:42 -0400

    use local swagger.json file for now

--- generate.sh
+++ generate.sh
@@ -1,8 +1,12 @@
 #/bin/bash
 java -jar swagger-codegen-cli-2.2.1.jar generate \
--i https://dev3.openmrs.org/openmrs/module/webservices/rest/swagger.json \
+-i swagger.json \
+\
 --git-repo-id openmrs-typescript-generator \
 --git-user-id openmrs \
 -l typescript-fetch \
 -t templates \
 -o output
+
+# TODO: when we're done, replace the -i param with the URL instead of using the local swagger.json file
+#-i https://dev3.openmrs.org/openmrs/module/webservices/rest/swagger.json \
