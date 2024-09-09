c63b9879d304ec8eeb0a76efa8863acaf56055d2
Chi Bong Ho <cbho@pih.org>
2024-09-09 07:20:08 -0400

    add generate.sh script

---
+++ generate.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+java -jar swagger-codegen-cli-2.2.1.jar generate \
+-i https://dev3.openmrs.org/openmrs/module/webservices/rest/swagger.json \
+--git-repo-id openmrs-typescript-generator \
+--git-user-id openmrs \
+-l typescript-fetch \
+-o output
